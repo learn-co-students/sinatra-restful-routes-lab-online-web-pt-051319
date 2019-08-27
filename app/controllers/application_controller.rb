836987aef1d4752cfab431e434cd7d7b9c396d42
scratchglory <scratchglory@gmail.com>
2019-08-27 10:08:14 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  # New Form
+  get '/recipes/new' do
+    erb :new
+  end
 
-end
+  # Index Action
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  # Show page
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  # Edit
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    # binding.pry
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  #   # Delete action
+  #   delete '/recipes/:id' do
+  #     @recipe = Recipe.find_by_id(params[:id])
+  #     binding.pry
+  #     @recipe.delete
+  #     redirect to '/recipes'
+  #   end
+
+  delete '/recipes/:id' do
+    #destroy action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+end # end of Class
